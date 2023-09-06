cc5e488c2e6c3c38ac4e9237ce5bccef59c90726
SanikaAlmeida <sanikaalmeida@gmail.com>
2023-09-06 09:25:23 +0530

    final commit

--- post.js
+++ post.js
@@ -3,8 +3,8 @@ const mongoose=require('mongoose')
 const url='mongodb://127.0.0.1:27017/PairDBex'
 
 const app=express()
-app.use(express.json())
-mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true})
+app.use(express.json())   //middleware that allows to handle json request body
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true})  //connect to MongoDB database
 const con=mongoose.connection
 
 con.on('open',()=>{
@@ -14,7 +14,7 @@ con.on('error', err => {
     console.error('connection error:',err)
   })
 
-const pairRouter=require('./route/pairs')
+const pairRouter=require('./route/pairs')  //import the module that handles the routing
 app.use('/pairs',pairRouter)
 
   app.listen(8080,()=>{
